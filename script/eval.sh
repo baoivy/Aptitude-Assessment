bc93945a9e805cae7104e5aba307594d586c17cc
baoivy <97684354+baoivy@users.noreply.github.com>
2024-03-28 12:38:54 +0700

    Update eval.sh

--- script/eval.sh
+++ script/eval.sh
@@ -1,5 +1,6 @@
 python src/predict.py \
     --input_file data/sample.json \
     --output_file output/result.jsonl \
-    --checkpoint_path bert-base-uncased \
+    --checkpoint_path /content/drive/MyDrive/AISIA-Assignment/lightning_logs/version_24/checkpoints/epoch=0-step=576.ckpt \
+    --model xlm-roberta-large \
     --predict True
